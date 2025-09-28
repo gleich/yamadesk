2958b03c5931e2085990f5f664defa4c2ce2e286
Matt Gleich <git@mattglei.ch>
2025-09-27 22:22:46 -0400

    feat: systemd service
    Signed-off-by: Matt Gleich <git@mattglei.ch>

--- Makefile
+++ Makefile
@@ -3,3 +3,10 @@
 
 sync:
 	rsync -avh --delete --progress --exclude 'target' --exclude '.git' . matt@gleichpi.local:~/yamadesk/
+
+install:
+	cargo install --path .
+	sudo mv yamadesk.service /etc/systemd/system/
+	sudo systemctl daemon-reload
+	sudo systemctl enable yamadesk.service
+	sudo systemctl start yamadesk.service
--- src/yamaha.rs
+++ src/yamaha.rs
@@ -5,26 +5,18 @@ use serde::{Deserialize, Serialize};
 use crate::config::Config;
 
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")] // can we maybe change this to snake case?
 pub struct StatusResponse {
-    #[serde(rename = "response_code")]
     pub response_code: i32,
     pub power: String,
     pub sleep: i32,
     pub volume: i32,
     pub mute: bool,
-    #[serde(rename = "max_volume")]
     pub max_volume: i32,
     pub input: String,
-    #[serde(rename = "input_text")]
     pub input_text: String,
-    #[serde(rename = "distribution_enable")]
     pub distribution_enable: bool,
-    #[serde(rename = "link_control")]
     pub link_control: String,
-    #[serde(rename = "link_audio_delay")]
     pub link_audio_delay: String,
-    #[serde(rename = "disable_flags")]
     pub disable_flags: i64,
 }
 
